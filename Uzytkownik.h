6c1772ba878a61521326d68193f10a4b2a75ab08
Milosz Anders <milosz.anders.programista@gmail.com>
2019-02-24 15:32:10 +0100

    Add registered user to vector

--- Uzytkownik.h
+++ Uzytkownik.h
@@ -9,4 +9,13 @@ int id;
 string login;
 string haslo;
 
+public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();
+
 };
