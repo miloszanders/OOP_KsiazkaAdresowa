2a8e0b32ee4e588cae9d9b2587f53911258c3af0
Milosz Anders <milosz.anders.programista@gmail.com>
2019-02-24 20:33:02 +0100

    Divide KsiazkaAdresowa to smaller class

---
+++ MetodyPomocnicze.cpp
@@ -0,0 +1,11 @@
+#include "MetodyPomocnicze.h"
+#include <sstream>
+
+
+string MetodyPomocnicze::konwerjsaIntNaString(int liczba)
+{
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
+}
---
+++ MetodyPomocnicze.h
@@ -0,0 +1,20 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+
+
+class MetodyPomocnicze
+{
+public:
+    static string konwerjsaIntNaString(int liczba);
+};
+
+
+#endif
